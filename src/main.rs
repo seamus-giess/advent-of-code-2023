b2dc47b883dee295d1fc34dab412bc13757ab49e
seamus-giess <seamusgiess@gmail.com>
2023-12-01 15:31:46 +1000

    feat: part 1 day 1

--- src/main.rs
+++ src/main.rs
@@ -51,7 +51,27 @@ fn main() {
         }
     }
 
-    println!("Data:\n\"{data_content}\"");
+    println!("Data:\n\"{data_content}\"\n");
 
+    let letterless = data_content.replace(
+        &[
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
+            'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        ][..],
+        "",
+    );
+
+    println!("{letterless}");
+
+    let exploded = letterless.split("\n");
+
+    let mut sum: u32 = 0;
+    exploded.for_each(|x| {
+        let first = x.chars().next().unwrap().to_digit(10).unwrap();
+        let last = x.chars().last().unwrap().to_digit(10).unwrap();
+        sum += (first * 10) + last;
+        println!("{}, {}, {}", sum, (first * 10), last)
+    });
+    println!("{}", sum);
     // Do a thing
 }
