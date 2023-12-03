233989f5cf6d5d29fe50a7828ce4fc8c3e80c22e
seamus-giess <seamusgiess@gmail.com>
2023-12-03 19:06:11 +1000

    feat: day 3 part 1

--- src/solvers/day_3_part_1.rs
+++ src/solvers/day_3_part_1.rs
@@ -1,5 +1,106 @@
-pub fn solve(data: &String) -> String {
-    let lines = data.split("\n");
+use std::str::FromStr;
 
-    return "sum".to_string();
+use itertools::Itertools;
+use regex::Regex;
+
+struct Grid {
+    rows: Vec<Vec<char>>,
+}
+impl Grid {
+    pub fn get_adjacent_numbers(&self, x: usize, y: usize) -> i32 {
+        let adjacent_coordinates = Vec::from([
+            (x - 1, y - 1),
+            (x, y - 1),
+            (x + 1, y - 1),
+            (x - 1, y),
+            (x + 1, y),
+            (x - 1, y + 1),
+            (x, y + 1),
+            (x + 1, y + 1),
+        ]);
+
+        return adjacent_coordinates
+            .into_iter()
+            .map(|(adj_x, adj_y)| {
+                return self.get_full_number(adj_x, adj_y);
+            })
+            .collect::<Vec<i32>>()
+            .into_iter()
+            .unique()
+            .fold(0, |acc, num| return acc + num);
+    }
+    fn get_full_number(&self, x: usize, y: usize) -> i32 {
+        let mut number_string = match self.rows[y][x] {
+            '.' => return 0,
+            char => char.to_string(),
+        };
+        let digits: Regex = Regex::new(r"[^0-9]").unwrap();
+
+        let mut right_shift: usize = 1;
+        loop {
+            match self.rows[y].get(x + right_shift) {
+                None | Some('.') => break, // Effectively "break;"
+                Some(symbol) => {
+                    if let Some(_is_not_num) = digits.find(symbol.to_string().as_str()) {
+                        break;
+                    };
+                    number_string.push(symbol.clone());
+                    right_shift += 1;
+                }
+            };
+        }
+
+        let mut left_shift: usize = 1;
+        loop {
+            match self.rows[y].get(x - left_shift) {
+                None | Some('.') => break,
+                Some(symbol) => {
+                    if let Some(_is_not_num) = digits.find(symbol.to_string().as_str()) {
+                        break;
+                    };
+                    number_string.insert(0, symbol.clone());
+                    left_shift += 1;
+                }
+            };
+        }
+        return number_string.parse().unwrap();
+    }
+}
+struct GridParseError;
+impl FromStr for Grid {
+    type Err = GridParseError;
+
+    fn from_str(data: &str) -> Result<Self, Self::Err> {
+        let lines = data.split("\n");
+
+        let grid: Vec<Vec<char>> = lines
+            .map(|line: &str| {
+                return line.chars().collect_vec();
+            })
+            .collect::<Vec<Vec<char>>>();
+
+        return Ok(Grid { rows: grid });
+    }
+}
+
+pub fn solve(data: &String) -> String {
+    let grid = match data.parse::<Grid>() {
+        Ok(grid) => grid,
+        _ => panic!("Failed to make the grid!"),
+    };
+
+    let symbols: Regex = Regex::new(r"[0-9.]").unwrap();
+
+    let mut sum = 0;
+    for (y, row) in grid.rows.clone().into_iter().enumerate() {
+        for (x, symbol) in row.into_iter().enumerate() {
+            if let Some(_is_skippable) = symbols.find(symbol.to_string().as_str()) {
+                continue;
+            };
+
+            sum += grid.get_adjacent_numbers(x, y);
+        }
+    }
+
+    return sum.to_string();
 }
--- src/solvers/day_3_part_2.rs
+++ src/solvers/day_3_part_2.rs
@@ -1,5 +1,5 @@
 pub fn solve(data: &String) -> String {
-    let lines = data.split("\n");
+    let _lines = data.split("\n");
 
     return "sum".to_string();
 }
