f9b6761c0f539b154c775de11918f9e57d3c0ba8
seamus-giess <seamusgiess@gmail.com>
2023-12-04 23:22:37 +1000

    feat: the single most verbose day 4 part 2

--- src/solvers/day_4_part_2.rs
+++ src/solvers/day_4_part_2.rs
@@ -1,20 +1,16 @@
-use std::collections::{HashMap, HashSet, VecDeque};
-
-use itertools::Itertools;
+use std::collections::{HashMap, HashSet};
 
 pub fn solve(data: &String) -> String {
-    let mut sum = 0;
     let card_strings = data.split('\n');
-    let mut won_cards: VecDeque<i32> = VecDeque::new();
-    let mut cards = HashMap::new();
+    let mut cards = Vec::new();
     card_strings.for_each(|card| {
-        let (card_id, card_numbers_string) = match card.split_once(": ") {
+        let (_card_id, card_numbers_string) = match card.split_once(": ") {
             Some((card_label, card_numbers_string)) => (
                 card_label
                     .split_whitespace()
                     .last()
                     .unwrap()
-                    .parse::<i32>()
+                    .parse::<usize>()
                     .unwrap(),
                 card_numbers_string,
             ),
@@ -27,33 +23,49 @@ pub fn solve(data: &String) -> String {
             ),
             _ => panic!("Can't split card and numbers!"),
         };
-        cards.insert(card_id, (winning_numbers, your_numbers));
+        cards.push((winning_numbers, your_numbers));
     });
-    let mut won_cards_count: HashMap<i32, i32> = HashMap::new();
-    let mut won_cards: VecDeque<i32> = VecDeque::new();
-    cards.keys().for_each(|key| {
-        won_cards.push_back(key.clone());
+    let mut card_queue_template: HashMap<usize, i32> = HashMap::new();
+    (1..cards.len() + 1).for_each(|key| {
+        card_queue_template.insert(key, 1);
     });
-    while won_cards.len() > 0 {
-        let next_card = won_cards.pop_front().unwrap();
-        won_cards_count.insert(
-            next_card,
-            match won_cards_count.get(&next_card) {
-                Some(count) => count + 1,
-                None => 1,
+    let mut index_tracker = 1;
+    let sum = cards
+        .into_iter()
+        .fold(
+            card_queue_template,
+            |card_queue, (winning_numbers, your_numbers)| {
+                let index = index_tracker.clone();
+                index_tracker += 1;
+
+                let mut new_cards = card_queue.clone();
+
+                let current_cards_count: i32 = match new_cards.get(&index) {
+                    Some(count) => count.clone(),
+                    None => 1,
+                };
+
+                let wins: i32 = winning_numbers.intersection(&your_numbers).count() as i32;
+                ((index + 1)..(index + 1 + wins as usize))
+                    .collect::<Vec<usize>>()
+                    .into_iter()
+                    .for_each(|index| {
+                        new_cards.insert(
+                            index,
+                            match new_cards.get(&index) {
+                                Some(count) => count + current_cards_count,
+                                None => 0 + current_cards_count,
+                            },
+                        );
+                    });
+
+                return new_cards;
             },
-        );
-        let (winning_numbers, your_numbers) = cards.get(&next_card).unwrap();
-        let wins = winning_numbers.intersection(your_numbers);
-        let wins_count: i32 = wins.count() as i32;
-        let mut counter = 0;
-        while counter < wins_count {
-            counter += 1;
-            won_cards.push_front(next_card + counter);
-        }
-    }
-    won_cards_count.into_iter().for_each(|(_key, cards_count)| {
-        sum += cards_count;
-    });
+        )
+        .into_iter()
+        .fold(0, |sum, (_index, count)| {
+            return sum + count;
+        });
+    assert_eq!(sum, 9881048);
     return sum.to_string();
 }
