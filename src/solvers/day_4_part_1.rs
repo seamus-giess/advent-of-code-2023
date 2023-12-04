8ae6952efea3108a2a487c93a38885e33eb00f21
seamus-giess <seamusgiess@gmail.com>
2023-12-04 19:00:11 +1000

    feat: day 4 part 1

--- src/solvers/day_4_part_1.rs
+++ src/solvers/day_4_part_1.rs
@@ -1,3 +1,39 @@
+use std::collections::HashSet;
+
 pub fn solve(data: &String) -> String {
-    return "sum".to_string();
+    let mut sum = 0;
+    let cards = data.split('\n');
+    cards.for_each(|card| {
+        let (card_id, card_numbers_string) = match card.split_once(": ") {
+            Some((card_label, card_numbers_string)) => (
+                card_label
+                    .split_whitespace()
+                    .last()
+                    .unwrap()
+                    .parse::<i32>()
+                    .unwrap(),
+                card_numbers_string,
+            ),
+            None => panic!("Can't split card and numbers!"),
+        };
+        let (winning_numbers, your_numbers) = match card_numbers_string.split_once(" | ") {
+            Some((wining_numbers_string, your_numbers_string)) => (
+                HashSet::<&str>::from_iter(wining_numbers_string.split_whitespace().into_iter()),
+                HashSet::<&str>::from_iter(your_numbers_string.split_whitespace().into_iter()),
+            ),
+            _ => panic!("Can't split card and numbers!"),
+        };
+        let wins: u32 = winning_numbers.intersection(&your_numbers).count() as u32;
+        println!(
+            "game: {}, wins: {}, intersects: {:?}, score {}",
+            card_id,
+            wins,
+            winning_numbers.intersection(&your_numbers),
+            1 * (2 as u32).pow(wins - 1)
+        );
+        if wins >= 1 {
+            sum += 1 * (2 as u32).pow(wins - 1);
+        }
+    });
+    return sum.to_string();
 }
