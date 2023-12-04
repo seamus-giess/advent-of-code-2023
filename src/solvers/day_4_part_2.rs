67a35d1b8d7b370ebff4943a5ddb8555bf23ab4d
seamus-giess <seamusgiess@gmail.com>
2023-12-04 19:32:39 +1000

    feat: whatever

--- src/solvers/day_4_part_2.rs
+++ src/solvers/day_4_part_2.rs
@@ -1,9 +1,11 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet, VecDeque};
 
 pub fn solve(data: &String) -> String {
     let mut sum = 0;
-    let cards = data.split('\n');
-    cards.for_each(|card| {
+    let card_strings = data.split('\n');
+    let mut won_cards: VecDeque<i32> = VecDeque::new();
+    let mut cards = HashMap::new();
+    card_strings.for_each(|card| {
         let (card_id, card_numbers_string) = match card.split_once(": ") {
             Some((card_label, card_numbers_string)) => (
                 card_label
@@ -23,17 +25,34 @@ pub fn solve(data: &String) -> String {
             ),
             _ => panic!("Can't split card and numbers!"),
         };
-        let wins: u32 = winning_numbers.intersection(&your_numbers).count() as u32;
-        println!(
-            "game: {}, wins: {}, intersects: {:?}, score {}",
-            card_id,
-            wins,
-            winning_numbers.intersection(&your_numbers),
-            1 * (2 as u32).pow(wins - 1)
-        );
-        if wins >= 1 {
-            sum += 1 * (2 as u32).pow(wins - 1);
+        cards.insert(card_id, (winning_numbers, your_numbers));
+    });
+    let mut won_cards_count = HashMap::new();
+    won_cards_count.insert(1, 1);
+    won_cards.push_back(1);
+    while won_cards.len() > 0 {
+        let next_card = won_cards.pop_front().unwrap();
+        println!("resolving: {:?}, stack: {:?}", next_card, won_cards);
+        let (winning_numbers, your_numbers) = cards.get(&next_card).unwrap();
+        let wins = winning_numbers.intersection(your_numbers);
+        wins.clone().for_each(|&won_card| {
+            won_cards_count.insert(
+                won_card.parse().unwrap(),
+                match won_cards_count.get(&won_card.parse().unwrap()) {
+                    Some(count) => count + 1,
+                    None => 1,
+                },
+            );
+        });
+        let wins_count: i32 = wins.count() as i32;
+        let mut counter = 0;
+        while counter < wins_count {
+            counter += 1;
+            won_cards.push_front(next_card + counter);
         }
+    }
+    won_cards_count.into_iter().for_each(|(_key, cards_count)| {
+        sum += cards_count;
     });
     return sum.to_string();
 }
